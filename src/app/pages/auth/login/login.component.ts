166a068b9768ee5e7afc17a904740301229336dc
Ahmed NourElDin Mohamed Awad <ahmed.nour199710@yahoo.com>
2025-05-30 15:31:13 +0300

    feat: Add authentication components for login, register, forgot password, and reset password

---
+++ src/app/pages/auth/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, inject, signal } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { AuthService } from '../../../core/services/auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule, ReactiveFormsModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  private authService = inject(AuthService);
+  private messageService = inject(MessageService);
+  private router = inject(Router);
+  username = signal('');
+  password = signal('');
+
+  onSubmit() {
+    this.authService.login();
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Welcome User',
+      detail: 'Logged in successfully',
+    });
+
+    this.router.navigate(['/products']);
+  }
+}
