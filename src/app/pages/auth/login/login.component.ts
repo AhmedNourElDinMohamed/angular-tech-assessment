aa38055357dd1740c1a143d3ea0dd04cdcbc29ac
Ahmed NourElDin Mohamed Awad <ahmed.nour199710@yahoo.com>
2025-05-30 19:47:43 +0300

    fix: login flow

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject, signal } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { AuthService } from '../../../core/services/auth.service';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { CommonModule, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, ReactiveFormsModule, RouterLink],
+  imports: [FormsModule, ReactiveFormsModule, RouterLink, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
@@ -14,9 +15,11 @@ export class LoginComponent {
   private authService = inject(AuthService);
   private messageService = inject(MessageService);
   private router = inject(Router);
-  username = signal('');
-  password = signal('');
 
+  user: { username: string; password: string } = {
+    username: '',
+    password: '',
+  };
   /**
    * Handles the submission of the login form
    */
